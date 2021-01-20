34f3973aee93c1ad22bc634a720d904a44b1da84
mmeerrccyy <58398502+mmeerrccyy@users.noreply.github.com>
2021-01-20 18:55:14 +0300

    added models in models.py, edited admin.py

--- products/admin.py
+++ products/admin.py
@@ -29,9 +29,22 @@ class TabletAdmin(admin.ModelAdmin):
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
+class SmartphoneAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(SubCategory.objects.filter(name='Смартфони'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
 admin.site.register(Category)
 admin.site.register(SubCategory)
 admin.site.register(Brand)
 admin.site.register(Notebook, NotebookAdmin)
 admin.site.register(PersonalComputer, PCAdmin)
 admin.site.register(Tablet, TabletAdmin)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(Cart)
+admin.site.register(CartProduct)
+admin.site.register(Customer)
+admin.site.register(Order)

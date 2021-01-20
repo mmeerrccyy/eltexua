194bbd676c7df8e0b54e8c3c8985dcca85d20616
mmeerrccyy <58398502+mmeerrccyy@users.noreply.github.com>
2021-01-20 17:49:30 +0300

    added supporting css, deleted STATIC_ROOT and edited test file index.html

--- eltexua/settings.py
+++ eltexua/settings.py
@@ -126,6 +126,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'static_dev'),
     os.path.join(BASE_DIR, 'static'),
 )

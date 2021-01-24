5a12dc70f321d8ffc2fb10b0e9797bafc3d286f5
mmeerrccyy <58398502+mmeerrccyy@users.noreply.github.com>
2021-01-24 14:13:11 +0300

    deleted fontawesome folder and added fontawesome-free package

--- eltexua/settings.py
+++ eltexua/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
+    'fontawesome-free',
 ]
 
 MIDDLEWARE = [

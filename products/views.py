573ca1ebb2fc733edfc29f5fc61be91cc0f19a22
mmeerrccyy <58398502+mmeerrccyy@users.noreply.github.com>
2021-01-24 12:30:41 +0300

    updated models.py, urls.py, views.py

--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Notebook
+from django.views.generic import DetailView
+
+from .models import *
+
+# from django.http import HttpResponse
+# from .models import Notebook
 # Create your views here.
 
 # def index(request):
@@ -9,3 +13,24 @@ from .models import Notebook
 
 def index(request):
     return render(request, 'products/wrapper.html')
+
+
+class ProductDetailView(DetailView):
+
+    CT_MODEL_MODEL_CLASS = {
+        'notebook': Notebook,
+        'pc': PersonalComputer,
+        'tablet': Tablet,
+        'smartphone': Smartphone,
+        'tvset': TVset,
+        'audio': Audio,
+    }
+
+    def dispatch(self, request, *args, **kwargs):
+        self.model = self.CT_MODEL_MODEL_CLASS[kwargs['ct_model']]
+        self.queryset = self.model._base_manager.all()
+        return super().dispatch(request, *args, **kwargs)
+
+    context_object_name = 'product'
+    template_name = 'products/product_detail.html'
+    slug_url_kwarg = 'slug'

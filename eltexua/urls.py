4effd5081fa7007cf33d587db646ebd6b00f6ca3
mmeerrccyy <58398502+mmeerrccyy@users.noreply.github.com>
2021-01-24 20:10:15 +0300

    edited settings.py and urls.py

--- eltexua/urls.py
+++ eltexua/urls.py
@@ -22,3 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

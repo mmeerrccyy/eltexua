a1c9ba92fac660c8f52b32ef7647491450dcd619
mmeerrccyy <58398502+mmeerrccyy@users.noreply.github.com>
2021-01-24 12:28:56 +0300

    added bootstrap, css, img, js

---
+++ static_cdn/js/main.js
@@ -0,0 +1,8 @@
+function button() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+function button2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}function button3() {
+    document.getElementById("myDropdown3").classList.toggle("show");
+}
